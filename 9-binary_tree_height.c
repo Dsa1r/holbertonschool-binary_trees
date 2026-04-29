ffc1d2bdedf9cfbc41a2a8f73a7e2ad1d8c36e5c
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-29 13:13:01 +0000

    Fix binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,21 +4,24 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node
  *
- * Return: Height of the tree
+ * Return: Height of the tree, or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left;
-	size_t right;
+	size_t left_height;
+	size_t right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	if (left > right)
-		return (left + 1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return (right + 1);
+	if (left_height > right_height)
+		return (left_height + 1);
+
+	return (right_height + 1);
 }
