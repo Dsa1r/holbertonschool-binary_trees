2a614be255189a0ff2b3d2e8d146befc52dbc285
dsa1r <Danamusaad22@gmail.com>
2026-05-02 15:18:23 +0000

    fix the error: put the right code to the wright file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,23 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the tree
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree
  *
- * Return: height, or 0 if tree is NULL
+ * Return: balance factor, or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_h = 0, right_h = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
-
-	if (left_h > right_h)
-		return (left_h + 1);
-
-	return (right_h + 1);
+	return ((int)binary_tree_height(tree->left) -
+			(int)binary_tree_height(tree->right));
 }
